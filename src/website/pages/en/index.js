b3fa4e2fd4c60d964ce3468de73ce4d3f825d433
Anton Fisher <a.fschr@gmail.com>
2018-03-02 13:01:21 -0800

    Update index.js

--- src/website/pages/en/index.js
+++ src/website/pages/en/index.js
@@ -75,6 +75,7 @@ class HomeSplash extends React.Component {
           <PromoSection>
             <Button href="https://github.com/Nexenta/edge-dev" target="_blank">Github</Button>
             <Button href="https://twitter.com/nexenta" target="_blank">Twitter</Button>
+            <Button href="https://join.slack.com/t/nexentaedge/shared_invite/enQtMzEwMjA5MTczNDU3LTVmNjk4NjEwNTVlYThjMjg4NWI0ZWM5NTBjNTE5YzgwZTFjYjhjMWFhMWY4NjYxYWI0YWJmOTFkNTY5MmI1YWI" target="_blank">Slack</Button>
             {/*<Button href={docUrl('doc1.html', language)}>Example Link</Button>*/}
           </PromoSection>
         </div>
