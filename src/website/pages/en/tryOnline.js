d6e20f96c840f500bc3d6860e5be213317ffac95
Dmitry Yusupov <dmitry@nexenta.com>
2018-03-02 21:58:01 -0800

    Update tryOnline.js

--- src/website/pages/en/tryOnline.js
+++ src/website/pages/en/tryOnline.js
@@ -7,7 +7,7 @@ class TryOnline extends React.Component {
         <script src="//katacoda.com/embed.js" />
         <div
           id="katakoda-nexenta-single-s3"
-          data-katacoda-id="/nexenta/single-s3"
+          data-katacoda-id="nexenta/single-s3"
           data-katacoda-color="004d7f"
           style={{minHeight: 600, paddingTop: 0}}
         />
