92b68608c797570de81e3d9cc3cfd424ef30197a
Anton Fisher <a.fschr@gmail.com>
2018-04-27 09:33:13 -0700

    feat: new home screen layout

--- src/website/pages/en/index.js
+++ src/website/pages/en/index.js
@@ -15,6 +15,18 @@ function pageUrl(page, language) {
   return siteConfig.baseUrl + (language ? language + '/' : '') + page;
 }
 
+class HeaderButton extends React.Component {
+  render() {
+    return (
+      <div style={{textAlign: 'center', margin: '0 15px'}}>
+        <a href={this.props.href} target={this.props.target}>
+          {this.props.children}
+        </a>
+      </div>
+    );
+  }
+}
+
 class Button extends React.Component {
   render() {
     return (
@@ -34,23 +46,30 @@ Button.defaultProps = {
 const SplashContainer = (props) => (
   <div
     className="homeContainer"
-    style={{
-      background: `url(${imgUrl('background.jpg')})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: '100% 100%'
-    }}
+    //style={{
+    //  background: `url(${imgUrl('background.jpg')})`,
+    //  backgroundRepeat: 'no-repeat',
+    //  backgroundSize: '100% 100%'
+    //}}
   >
     <div className="homeSplashFade">
-      <div className="wrapper homeWrapper">{props.children}</div>
+      <div
+        className="wrapper homeWrapper"
+        style={{
+          display: 'flex'
+        }}
+      >
+        {props.children}
+      </div>
     </div>
   </div>
 );
 
 const ProjectTitle = () => (
   <h2 className="projectTitle">
-    Discover NexentaEdge
+    Connect to NexentaEdge
     <small>
-      <i>{siteConfig.tagline}</i>
+      <i>Fast, feature rich and easy to use File, Block and Object storage for your Cloud-Native Applications</i>
     </small>
   </h2>
 );
@@ -67,89 +86,107 @@ class HomeSplash extends React.Component {
   render() {
     return (
       <SplashContainer>
-        <div className="inner">
+        {/*<div className="inner" style={{padding: '40px 0'}}>*/}
+        <section
+          style={{
+            fontSize: '16px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            paddingTop: 25
+          }}
+        >
           <ProjectTitle />
-          <PromoSection>
-            <Button href="https://github.com/Nexenta/edge-dev" target="_blank">
-              <i className="fab fa-github fa-lg fa-fw" /> Github
-            </Button>
-            <Button href="https://twitter.com/nexenta" target="_blank">
-              <i className="fab fa-twitter fa-lg fa-fw" /> Twitter
-            </Button>
-            <Button
+          <section
+            className="headerButtons"
+            style={{display: 'flex', justifyContent: 'center', margin: '5px 0 40px 0'}}
+          >
+            <HeaderButton href="https://nexenta.com/products/nexentaedge" target="_blank">
+              <img src={imgUrl('logo-nexenta-edge.png')} style={{height: 64, marginBottom: 0}}/>
+              <br />Product Page
+            </HeaderButton>
+            <HeaderButton href="https://github.com/Nexenta/edge-dev" target="_blank">
+              <i className="fab fa-github fa-4x fa-fw" />
+              <br />Github
+            </HeaderButton>
+            <HeaderButton
               href="https://join.slack.com/t/nexentaedge/shared_invite/enQtMzEwMjA5MTczNDU3LTVmNjk4NjEwNTVlYThjMjg4NWI0ZWM5NTBjNTE5YzgwZTFjYjhjMWFhMWY4NjYxYWI0YWJmOTFkNTY5MmI1YWI"
               target="_blank"
             >
-              <i className="fab fa-slack fa-lg fa-fw" /> Slack
-            </Button>
-            <Button href="https://groups.google.com/forum/#!forum/nexentaedge-users" target="_blank">
-              <i className="fab fa-google fa-lg fa-fw" /> Google Group
-            </Button>
-            <Button href={pageUrl('tryOnline.html')}>
-              <i className="fas fa-globe fa-lg fa-fw" /> Try Online
-            </Button>
-          </PromoSection>
-        </div>
+              <i className="fab fa-slack fa-4x fa-fw" />
+              <br />Slack
+            </HeaderButton>
+            <HeaderButton href="https://twitter.com/nexenta" target="_blank">
+              <i className="fab fa-twitter fa-4x fa-fw" />
+              <br />Twitter
+            </HeaderButton>
+            <HeaderButton href="https://groups.google.com/forum/#!forum/nexentaedge-users" target="_blank">
+              <i className="fab fa-google fa-4x fa-fw" />
+              <br />Google Group
+            </HeaderButton>
+          </section>
+        </section>
+        {/*</div>*/}
       </SplashContainer>
     );
   }
 }
 
-const Block = (props) => (
-  <Container padding={['bottom', 'top']} id={props.id} background={props.background}>
-    <GridBlock align="center" contents={props.children} layout={props.layout} />
-  </Container>
-);
+//const Block = (props) => (
+//  <Container padding={['bottom', 'top']} id={props.id} background={props.background}>
+//    <GridBlock align="center" contents={props.children} layout={props.layout} />
+//  </Container>
+//);
 
-const Features = () => (
-  <Block layout="fourColumn">
-    {[
-      {
-        content: `<ul align="left">
-<li>Advanced Versioned S3 Object Append and RW ”Object as File” access
-<li>S3 Object as a Key-Value database, including integrations with Caffe, TensorFlow, Spark, Kafka, etc
-<li>High-performance Versioned S3 Object Stream Session ”RW”, including FUSE library to mount an object
-<li>Management API for Snapshots and Clones, including Bucket instantaneous snapshots
-<li>Transparent NFS to/from S3 bucket access, “ingest via NFS, read via S3” or vice-versa
-</ul>
-<a class="button" href="https://github.com/Nexenta/edge-dev" target="_blank">Learn More</a>
-`,
-        image: imgUrl('machine_learning_logo.png'),
-        imageAlign: 'top',
-        title: 'Optimized for AI/ML frameworks'
-      },
-      {
-        content: `<ul align="left">
-<li>Deployed as containers and managed using standard container tools
-<li>Micro-services for data access to File ”NFS”, Block ”iSCSI/NBD”, Object ”S3/SWIFT” and NOSQL database
-<li>Global inline deduplication and compression
-<li>Global Name space and built-in Multi-Tenancy
-<li>Enterprise class feature set with built-in data reduction, snapshots, clones and QoS
-<li>Easy administration with DevOps familiar tools, e.g. (upcoming Rook.IO integration)
-</ul>
-<a class="button" href="https://github.com/Nexenta/edge-dev" target="_blank">Learn More</a>
-`,
-        image: imgUrl('microservices_logo.png'),
-        imageAlign: 'top',
-        title: 'Multi-Protocol Persistent Volumes'
-      },
-      {
-        content: `<ul align="left">
-<li>S3 Object as a Key-Value storage database with easy access via Node.JS, Java, Python, C/C++ APIs
-<li>Advanced S3 Object Append and RW access, High-performance versioning
-<li>High-performance S3 Object Stream Session ”POSIX mountable”
-<li>Management API for Snapshots and Clones where any types of objects and buckets can be snapshotted and cloned
-<li>Dynamic data placement and automatic load balancing
-</ul>
-<a class="button" href="https://github.com/Nexenta/edge-dev" target="_blank">Learn More</a>
-`,
-        image: imgUrl('big_data_logo.png'),
-        imageAlign: 'top',
-        title: 'Edge-X S3 API for Big Data and Analytics'
-      }
-    ]}
-  </Block>
-);
+//const Features = () => (
+//  <Block layout="fourColumn">
+//    {[
+//      {
+//        content: `<ul align="left">
+//<li>Advanced Versioned S3 Object Append and RW ”Object as File” access
+//<li>S3 Object as a Key-Value database, including integrations with Caffe, TensorFlow, Spark, Kafka, etc
+//<li>High-performance Versioned S3 Object Stream Session ”RW”, including FUSE library to mount an object
+//<li>Management API for Snapshots and Clones, including Bucket instantaneous snapshots
+//<li>Transparent NFS to/from S3 bucket access, “ingest via NFS, read via S3” or vice-versa
+//</ul>
+//<a class="button" href="https://github.com/Nexenta/edge-dev" target="_blank">Learn More</a>
+//`,
+//        image: imgUrl('machine_learning_logo.png'),
+//        imageAlign: 'top',
+//        title: 'Optimized for AI/ML frameworks'
+//      },
+//      {
+//        content: `<ul align="left">
+//<li>Deployed as containers and managed using standard container tools
+//<li>Micro-services for data access to File ”NFS”, Block ”iSCSI/NBD”, Object ”S3/SWIFT” and NOSQL database
+//<li>Global inline deduplication and compression
+//<li>Global Name space and built-in Multi-Tenancy
+//<li>Enterprise class feature set with built-in data reduction, snapshots, clones and QoS
+//<li>Easy administration with DevOps familiar tools, e.g. (upcoming Rook.IO integration)
+//</ul>
+//<a class="button" href="https://github.com/Nexenta/edge-dev" target="_blank">Learn More</a>
+//`,
+//        image: imgUrl('microservices_logo.png'),
+//        imageAlign: 'top',
+//        title: 'Multi-Protocol Persistent Volumes'
+//      },
+//      {
+//        content: `<ul align="left">
+//<li>S3 Object as a Key-Value storage database with easy access via Node.JS, Java, Python, C/C++ APIs
+//<li>Advanced S3 Object Append and RW access, High-performance versioning
+//<li>High-performance S3 Object Stream Session ”POSIX mountable”
+//<li>Management API for Snapshots and Clones where any types of objects and buckets can be snapshotted and cloned
+//<li>Dynamic data placement and automatic load balancing
+//</ul>
+//<a class="button" href="https://github.com/Nexenta/edge-dev" target="_blank">Learn More</a>
+//`,
+//        image: imgUrl('big_data_logo.png'),
+//        imageAlign: 'top',
+//        title: 'Edge-X S3 API for Big Data and Analytics'
+//      }
+//    ]}
+//  </Block>
+//);
 
 class Index extends React.Component {
   render() {
@@ -159,26 +196,51 @@ class Index extends React.Component {
       <div>
         <HomeSplash language={language} />
         <div className="mainContainer">
-          <Features />
-          <section className="container" style={{backgroundColor: '#e6e6e6', paddingBottom: 40}}>
-            <section
-              className="wrapper"
-              style={{paddingTop: 50, display: 'flex', justifyContent: 'space-around'}}
-            >
-              <section style={{textAlign: 'center'}}>
-                <img src={imgUrl('logo-openstack.png')} style={{height: 70}} />
-                <br />
-                <a href="/openstack/" style={{display: 'inline-block', marginTop: 20}}>
-                  Openstack Drivers
-                </a>
+          {/*<Features />*/}
+          <section className="container" style={{backgroundColor: '#f3f3f3', padding: '0 0 50px 0'}}>
+            <section className="band band-container">
+              <section style={{paddingTop: 40}}>
+                <h2>Try Online</h2>
+                <p>Deploy NexentaEdge cluster online, no hardware required</p>
+                <Button href="https://www.katacoda.com/courses/nexenta/single-s3">
+                  <i className="fas fa-globe fa-lg fa-fw" /> Try Online
+                </Button>
               </section>
-              <section style={{textAlign: 'center'}}>
-                <img src={imgUrl('logo-docker.svg')} style={{height: 70}} />
-                <br />
-                <a href="/docker/" style={{display: 'inline-block', marginTop: 20}}>
-                  Openstack & K8s Drivers
-                </a>
+              <img
+                src={imgUrl('katacoda-nedge.png')}
+                style={{
+                  height: 220,
+                  border: '1px solid rgb(221, 221, 221)',
+                  boxShadow: '3px 3px 4px 1px #ddd',
+                  marginLeft: 20,
+                  marginTop: 15
+                }}
+              />
+            </section>
+          </section>
+          <section className="container" style={{backgroundColor: '#e6e6e6', padding: '35px 0 50px 0'}}>
+            <section className="band band-container">
+              <img src={imgUrl('logo-docker.svg')} style={{height: 120, marginRight: 20, marginTop: 5}} />
+              <section>
+                <h2>Docker Drivers</h2>
+                <p>Get the latest volume plugins and drivers for block and NFS for NexentaEdge</p>
+                <section style={{display: 'flex'}}>
+                  <Button href="https://github.com/Nexenta/nedge-docker-volume">Docker v1.13 Volume Driver</Button>
+                  <Button href="https://github.com/Nexenta/nedge-docker-nfs">Docker v17 NFS Volume Plugin</Button>
+                </section>
               </section>
+            </section>
+          </section>
+          <section className="container" style={{backgroundColor: '#f5f5f5', padding: '35px 0 50px 0'}}>
+            <section className="band band-container">
+              <section>
+                <h2>OpenStack Drivers</h2>
+                <p>Get the latest Cinder block and file drivers for NexentaEdge</p>
+                <Button href="https://github.com/Nexenta/cinder/tree/stable/queens/cinder/volume/drivers/nexenta">
+                  OpenStack Drivers
+                </Button>
+              </section>
+              <img src={imgUrl('logo-openstack.png')} style={{height: 100, marginLeft: 20, marginTop: 15}} />
             </section>
           </section>
         </div>
--- src/website/siteConfig.js
+++ src/website/siteConfig.js
@@ -1,12 +1,12 @@
 const siteConfig = {
-  title: '',
+  title: 'NexentaEdge',
   tagline: 'Fast, feature rich and easy to use File, Block and Object storage for your Cloud-Native Applications',
+  disableHeaderTitle: true,
   url: 'http://nexentaedge.io',
   baseUrl: '/',
   projectName: 'nexentaedge.github.io', // or set an env variable PROJECT_NAME
   organizationName: 'nexentaedge', // or set an env variable ORGANIZATION_NAME
   headerLinks: [
-    {href: 'https://groups.google.com/forum/#!forum/nexentaedge-users', label: 'Ask us'},
     {doc: 'introduction', label: 'Documentation'},
     {blog: true, label: 'Blog'}
   ],
@@ -25,17 +25,20 @@ const siteConfig = {
     primaryColor: '#f37121',
     secondaryColor: '#c6c6c6'
   },
-  //fonts: {
-  //  myFont: [
-  //    "Open Sans",
-  //    "-apple-system",
-  //    "system-ui"
-  //  ]
-  //  myOtherFont: [
-  //    "-apple-system",
-  //    "system-ui"
-  //  ]
-  //},
+  fonts: {
+    myFont: [
+      "Open Sans",
+      "-apple-system",
+      "system-ui",
+      "sans-serif"
+    ],
+    myOtherFont: [
+      "Open Sans",
+      "-apple-system",
+      "system-ui",
+      "sans-serif"
+    ]
+  },
   copyright: `Copyright © ${new Date().getFullYear()} Nexenta Systems`,
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
@@ -46,6 +49,9 @@ const siteConfig = {
     'https://use.fontawesome.com/releases/v5.0.8/js/all.js',
     'https://unpkg.com/mermaid@8.0.0-rc.6/dist/mermaid.min.js',
     '/js/main.js'
+  ],
+  stylesheets: [
+      'https://fonts.googleapis.com/css?family=Open+Sans'
   ],
   // You may provide arbitrary config keys to be used as needed by your template.
   repoUrl: 'https://github.com/Nexenta/nedge-dev',
